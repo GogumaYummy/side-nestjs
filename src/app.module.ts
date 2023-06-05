f001e2daab7c0dc0bd73561717f019e5a9e8e93e
GogumaYummy <sora1211x@gmail.com>
2023-06-05 09:23:54 +0900

    feat: 환경 변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ConfigModule.forRoot()],
+  //TODO: 이후 배포 환경에서의 환경 변수는 따로 지정하게 할 것
   controllers: [AppController],
   providers: [AppService],
 })
