97887bf6d4d9325b66a5004a0f6dcc4048036132
GogumaYummy <sora1211x@gmail.com>
2023-06-07 19:39:20 +0900

    feat: 환경 변수의 유효성 검사 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validate } from './config-validation';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, ConfigModule.forRoot()],
+  imports: [UserModule, ConfigModule.forRoot({ validate })],
   //TODO: 이후 배포 환경에서의 환경 변수는 따로 지정하게 할 것
   controllers: [AppController],
   providers: [AppService],
--- src/config-validation.ts
+++ src/config-validation.ts
@@ -1,9 +1,21 @@
-import { IsDefined, IsNumber } from 'class-validator';
-import { Transform } from 'class-transformer';
+import { plainToInstance, Transform } from 'class-transformer';
+import { IsDefined, IsNumber, validateSync } from 'class-validator';
 
 class AppConfig {
   @IsNumber()
   @IsDefined()
   @Transform(({ value }) => parseInt(value, 10))
   PORT!: number;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const appConfig = plainToInstance(AppConfig, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(appConfig);
+
+  if (errors.length) throw new Error(errors.toString());
+
+  return appConfig;
 }
