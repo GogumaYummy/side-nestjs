1d31883b31e633f3e596e8d7d0869c5e9f497308
GogumaYummy <sora1211x@gmail.com>
2023-06-27 15:37:21 +0900

    feat: DB 연결 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validate } from './env.validation';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [UserModule, ConfigModule.forRoot({ validate })],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ validate }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: configService.get('NODE_ENV') === 'development',
+      }),
+    }),
+    PostModule,
+  ],
   //TODO: 이후 배포 환경에서의 환경 변수는 따로 지정하게 할 것
   controllers: [AppController],
   providers: [AppService],
