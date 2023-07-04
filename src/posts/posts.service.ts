025560e08fad20138271351a1eb04ba75ee12769
GogumaYummy <sora1211x@gmail.com>
2023-07-04 13:52:02 +0900

    feat: CRUD 메서드 추가

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
+
+  async createPost(title: string, content: string): Promise<Post> {
+    const post = new Post();
+
+    post.title = title;
+    post.content = content;
+
+    return await this.postsRepository.save(post);
+  }
+
+  async getPosts(): Promise<Post[]> {
+    return await this.postsRepository.find();
+  }
+
+  async getPostById(postId: number): Promise<Post> {
+    const post = await this.postsRepository.findOneBy({ postId });
+
+    if (!post) throw new Error('게시글을 찾을 수 없습니다');
+
+    return post;
+  }
+
+  async updatePost(
+    postId: number,
+    title: string,
+    content: string,
+  ): Promise<Post> {
+    const post = await this.postsRepository.findOneBy({ postId });
+
+    if (!post) throw new Error('게시글을 찾을 수 없습니다');
+
+    post.title = title;
+    post.content = content;
+
+    return await this.postsRepository.save(post);
+  }
+
+  async deletePost(postId): Promise<void> {
+    const post = await this.postsRepository.findOneBy({ postId });
+
+    if (!post) throw new Error('게시글을 찾을 수 없습니다');
+
+    await this.postsRepository.delete(post);
+  }
+}
