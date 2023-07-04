9eb544755e6bb8bd96fecf7270c274d1f531828d
GogumaYummy <sora1211x@gmail.com>
2023-07-04 14:50:24 +0900

    feat: CRUD 메서드 추가

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+import { Post as PostEntity } from './post.entity';
+
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  createPost(
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ): Promise<PostEntity> {
+    return this.postsService.createPost(title, content);
+  }
+
+  @Get()
+  getPosts(): Promise<PostEntity[]> {
+    return this.postsService.getPosts();
+  }
+
+  @Get()
+  getPostById(@Body('postId') postId: number): Promise<PostEntity> {
+    return this.postsService.getPostById(postId);
+  }
+
+  @Put()
+  updatePost(
+    @Body('postId') postId: number,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    return this.postsService.updatePost(postId, title, content);
+  }
+
+  @Delete()
+  deletePost(@Body('postId') postId: number) {
+    return this.postsService.deletePost(postId);
+  }
+}
