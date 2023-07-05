0e3e37e43d93961ffe09180bc7f7bd15b873a9fd
GogumaYummy <sora1211x@gmail.com>
2023-07-05 18:35:21 +0900

    feat: 유효성 검사 데코레이터 추가

--- src/posts/posts.dto.ts
+++ src/posts/posts.dto.ts
@@ -1,9 +1,19 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   content: string;
 }
 
 export class UpdatePostDto {
+  @IsString()
   title?: string;
+
+  @IsString()
   content?: string;
 }
