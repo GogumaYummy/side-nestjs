8f7b90c57531a91d817afd736216fe32158f5af7
GogumaYummy <sora1211x@gmail.com>
2023-07-06 13:31:08 +0900

    refactor: 오류를 발생시키던 부분을 Not Found 예외로 변경

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Post } from './post.entity';
@@ -27,7 +27,7 @@ export class PostsService {
   async getPostById(postId: number): Promise<Post> {
     const post = await this.postsRepository.findOneBy({ postId });
 
-    if (!post) throw new Error('게시물을 찾을 수 없습니다');
+    if (!post) throw new NotFoundException('게시물을 찾을 수 없습니다');
 
     return post;
   }
@@ -38,7 +38,7 @@ export class PostsService {
   ): Promise<Post> {
     const post = await this.postsRepository.findOneBy({ postId });
 
-    if (!post) throw new Error('게시물을 찾을 수 없습니다');
+    if (!post) throw new NotFoundException('게시물을 찾을 수 없습니다');
 
     post.title = updatePostDto.title;
     post.content = updatePostDto.content;
@@ -49,7 +49,7 @@ export class PostsService {
   async deletePost(postId: number): Promise<void> {
     const post = await this.postsRepository.findOneBy({ postId });
 
-    if (!post) throw new Error('게시물을 찾을 수 없습니다');
+    if (!post) throw new NotFoundException('게시물을 찾을 수 없습니다');
 
     await this.postsRepository.delete(post);
   }
