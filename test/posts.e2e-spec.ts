b9c88acda671c81953b85e7a776b229c86746e1b
GogumaYummy <sora1211x@gmail.com>
2023-07-28 17:21:01 +0900

    test: POST /posts 테스트 코드 작성

--- test/posts.e2e-spec.ts
+++ test/posts.e2e-spec.ts
@@ -1,6 +1,7 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
 import { PostsModule } from '../src/posts/posts.module';
 
 describe('PostsController (e2e)', () => {
@@ -25,5 +26,34 @@ describe('PostsController (e2e)', () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  describe('POST /posts', () => {
+    it('게시물이 성공적으로 작성된 경우', async () => {
+      const createPostDto = {
+        title: 'Lorem Ipsum',
+        content: 'Lorem ipsum dolor sit amet',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/posts')
+        .send(createPostDto)
+        .expect(HttpStatus.CREATED);
+
+      const { postId, title, content } = response.body;
+
+      expect(postId).toBe(1);
+      expect(title).toBe(createPostDto.title);
+      expect(content).toBe(createPostDto.content);
+    });
+
+    it('요청 본문이 잘못된 경우', async () => {
+      const createPostDto = {};
+
+      await request(app.getHttpServer())
+        .post('/posts')
+        .send(createPostDto)
+        .expect(HttpStatus.BAD_REQUEST);
+    });
   });
 });
